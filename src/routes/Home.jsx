6c3a45ce4407944bcefdb1d530826c36049e569b
Mikko <realmiks27@gmail.com>
2024-04-22 10:34:50 -0700

    Add search post

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,6 +6,9 @@ import Post from "../components/Post";
 const Home = () => {
   const data = useLoaderData();
   const [displayedData, setDisplayedData] = useState([...data]);
+  const [input, setInput] = useState("");
+  // by default, the latest should be clicked
+  // const [clicked, setClicked] = useState("");
   console.log(data);
 
   const handleClick = async(e) => {
@@ -16,7 +19,7 @@ const Home = () => {
         .select()
         .order("created_at", { ascending: false });
       setDisplayedData(newData);
-    } else if (e.target.textContent === "Upvotes") {
+    } else if (e.target.textContent === "Popular") {
       const { data: newData } = await supabase
         .from("hoop-talk-posts")
         .select()
@@ -30,22 +33,54 @@ const Home = () => {
       setDisplayedData(newData);
     }
   };
+  
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    console.log("submit");
+    const { data: newData } = await supabase
+      .from("hoop-talk-posts")
+      .select()
+      .eq("title", input);
+    if (newData.length === 0) {
+      setDisplayedData([]);
+    } else {
+      setDisplayedData(newData);  
+    }
+    setInput("");
+  };
 
   // const 
   return (
     <div className="">
       <div className="flex gap-8">
-        <button onClick={handleClick}>Latest</button>
-        <button onClick={handleClick}>Upvotes</button>
-        <button onClick={handleClick}>Oldest</button>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Search post" value={input} 
+            onChange={(e) => setInput(e.target.value)}
+            className="border-2 border-solid border-black"/>
+        </form>
+        <button onClick={handleClick} 
+          className="border-2 border-solid border-black">
+            Latest
+        </button>
+        <button onClick={handleClick}
+          className="border-2 border-solid border-black">
+            Popular
+        </button>
+        <button onClick={handleClick}
+          className="border-2 border-solid border-black">
+            Oldest
+        </button>
 
       </div>
-      {displayedData && displayedData.map((post) => {
-        return (
-          <Post key={post.id} title={post.title} 
-            date={post.created_at} upvotes={post.upvotes} id={post.id}/>
-        );
-      })}
+
+      {displayedData.length === 0 
+        ? <div className=""><i>Post with that title not found!</i></div> 
+        : displayedData.map((post) => {
+          return (
+            <Post key={post.id} title={post.title} 
+              date={post.created_at} upvotes={post.upvotes} id={post.id}/>
+          );
+        })}
     </div>
   );
 };
