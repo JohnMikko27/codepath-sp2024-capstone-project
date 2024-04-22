8ac8477f3e08c4f7d48deaa6aee56323ca25a6f9
Mikko <realmiks27@gmail.com>
2024-04-22 10:02:26 -0700

    Add filters

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import { formatDate } from "../utils/utils";
 
-const Post = ({ title, description, date, upvotes, id }) => {
+const Post = ({ title, date, upvotes, id }) => {
   return (
     <div className="border-black border-solid border-2">
       <Link to={`/details/${id}`}>
         <div>{formatDate(date)}</div>
         <div>{title}</div>
-        <div>{description}</div>
+        {/* <div>{description}</div> */}
         <div>upvotes: {upvotes}</div>
       </Link>
     </div>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,48 @@
 import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import supabase from "../utils/client";
 import Post from "../components/Post";
 
 const Home = () => {
   const data = useLoaderData();
+  const [displayedData, setDisplayedData] = useState([...data]);
   console.log(data);
+
+  const handleClick = async(e) => {
+    // console.log("handleLatest clicked");
+    if (e.target.textContent === "Latest") {
+      const { data: newData } = await supabase
+        .from("hoop-talk-posts")
+        .select()
+        .order("created_at", { ascending: false });
+      setDisplayedData(newData);
+    } else if (e.target.textContent === "Upvotes") {
+      const { data: newData } = await supabase
+        .from("hoop-talk-posts")
+        .select()
+        .order("upvotes", { ascending: false });
+      setDisplayedData(newData);
+    } else if (e.target.textContent === "Oldest") {
+      const { data: newData } = await supabase
+        .from("hoop-talk-posts")
+        .select()
+        .order("created_at", { ascending: true });
+      setDisplayedData(newData);
+    }
+  };
+
+  // const 
   return (
     <div className="">
-      {data && data.map((post) => {
+      <div className="flex gap-8">
+        <button onClick={handleClick}>Latest</button>
+        <button onClick={handleClick}>Upvotes</button>
+        <button onClick={handleClick}>Oldest</button>
+
+      </div>
+      {displayedData && displayedData.map((post) => {
         return (
-          <Post key={post.id} title={post.title} description={post.description} 
+          <Post key={post.id} title={post.title} 
             date={post.created_at} upvotes={post.upvotes} id={post.id}/>
         );
       })}
