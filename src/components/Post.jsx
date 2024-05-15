cf45c4e723f4556ecbb6c1828dcf3184031ea388
Mikko <realmiks27@gmail.com>
2024-05-15 10:34:21 -0700

    Update styling of Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,11 +4,13 @@ import { formatDate } from "../utils/utils";
 const Post = ({ title, date, upvotes, id, username}) => {
   // it doesn't fully allow you to click the entire post, clicking near the outer part won't actually click
   return (
-    <div className="border-gray-400 border-solid border-1 bg-white 
-    px-8 py-4 rounded-sm hover:bg-slate-200  transition-all duration-200">
-      <div>author: {username}</div>
-      <Link className="" to={`/details/${id}`}>
-        <div className=" text-xs text-gray-400">{formatDate(date)}</div>
+    <div className="post border-gray-400 border-solid border-1 bg-white 
+    px-8 py-4 rounded-sm ">
+      <Link className="post-link" to={`/details/${id}`}>
+        <div className="flex items-center justify-between">
+          <div className="text-xs text-gray-400">Posted by: {username}</div>
+          <div className=" text-xs text-gray-400">{formatDate(date)}</div>
+        </div>
         <div className="text-xl">{title}</div>
         {/* <div>{description}</div> */}
         <div>upvotes: {upvotes}</div>
