ad922270c0312b9cda33251aa68d0279aec7af86
Mikko <realmiks27@gmail.com>
2024-11-19 17:45:38 -0800

    add production stats api url

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,15 +2,19 @@ import { Params } from "react-router-dom";
 import supabase from "./client";
 
 const playerLoader = async({ params }: { params: Params<string> }) => {
+  const statsApiUrl = import.meta.env.PROD 
+    ? import.meta.env.VITE_APP_PROD_STATS_API_URL 
+    : import.meta.env.VITE_APP_DEV_STATS_API_URL;
+    
   try {
-    const playerResponse = await fetch(`http://localhost:8000/players/${params.playerName}`);
+    const playerResponse = await fetch(`${statsApiUrl}/players/${params.playerName}`);
     const playerInfo = await playerResponse.json();
   
     // should check if theres an error and then send that error
-    const regSeasonResponse = await fetch(`http://localhost:8000/stats/${params.playerName}/regseason`);
+    const regSeasonResponse = await fetch(`${statsApiUrl}/stats/${params.playerName}/regseason`);
     const regSeasonStats = await regSeasonResponse.json();
 
-    const postSeasonResponse = await fetch(`http://localhost:8000/stats/${params.playerName}/postseason`);
+    const postSeasonResponse = await fetch(`${statsApiUrl}/stats/${params.playerName}/postseason`);
     const postSeasonStats = await postSeasonResponse.json();
     if (playerInfo.status === false || regSeasonStats.status === false || postSeasonStats.status === false) {
       return { status: false, message: "Player not found", playerInfo: {}, regSeasonStats: {}, postSeasonStats: {} };
