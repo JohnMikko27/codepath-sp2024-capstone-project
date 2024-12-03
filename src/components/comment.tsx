2c21919546e282c192433d7b4113aa09f4daf274
Mikko <realmiks27@gmail.com>
2024-12-02 17:42:50 -0800

    add edit comment functionality

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,8 +1,8 @@
 import { CommentType } from "@/utils/types";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { DateTime as dt } from "ts-luxon";
 import { UserType } from "@/utils/types";
-import { /*Pencil,*/ Trash2 } from "lucide-react";
+import { Pencil, Trash2 } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 import { useNavigate } from "react-router-dom";
 import {
@@ -16,11 +16,15 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 export default function Comment({ comment, postId } : { comment: CommentType, postId: number }) {
   const [author, setAuthor] = useState<UserType>({id: 0, username: "", password: "", createdAt: "", posts: [], comments: []});
-  const formattedDate = dt.fromISO(comment.createdAt).toLocaleString(dt.DATE_SHORT);
+  const [comm, setComm] = useState(comment);
+  const formattedDate = dt.fromISO(comm.createdAt).toLocaleString(dt.DATE_SHORT);
+  const [isEdit, setIsEdit] = useState(false);
+  const [input, setInput] = useState(comm.content);
   const navigate = useNavigate();
   const { toast } = useToast();
   const env = import.meta.env.PROD 
@@ -31,7 +35,7 @@ export default function Comment({ comment, postId } : { comment: CommentType, po
     let first = true;
     const fetchAuthor = async() => {
       const token = localStorage.getItem("token");
-      const response = await fetch(env + `/users/${comment.authorId}`, {
+      const response = await fetch(env + `/users/${comm.authorId}`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -53,7 +57,7 @@ export default function Comment({ comment, postId } : { comment: CommentType, po
   const handleDelete = async() => {
     const token = localStorage.getItem("token");
     try {
-      const response = await fetch(env + `/posts/${postId}/comments/${comment.id}`, {
+      const response = await fetch(env + `/posts/${postId}/comments/${comm.id}`, {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
@@ -70,7 +74,31 @@ export default function Comment({ comment, postId } : { comment: CommentType, po
       console.log(e);
     }
   };
-     
+
+  const handleEdit = async(e: React.FormEvent) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch(env + `/posts/${postId}/comments/${comm.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization" : `${token}`,
+        }, 
+        body: JSON.stringify({ content: input })
+      });
+      const data = await response.json();
+      if (data.status !== 200) {
+        return;
+      }
+      toast({ title: data.message, className: "bg-slate-950 text-white" });
+      setIsEdit(false);
+      setComm({...data.comment});
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return(
     <div className="grid gap-1 bg-white border-1 border-gray-400 border-solid 
     px-4 py-2 rounded-md"
@@ -80,33 +108,45 @@ export default function Comment({ comment, postId } : { comment: CommentType, po
         <div>{author && author.username}</div>
         <div>{formattedDate}</div>
       </div>
-      <div className="flex justify-between">
-        <div>{comment.content}</div>
-        <div className="flex items-center gap-4">
-          {/* <Pencil size={18} onClick={} /> */}
-          { 
-            <AlertDialog>
-              <AlertDialogTrigger>
-                <Trash2 size={18}  className="hover:cursor-pointer"/>
+      {
+        !isEdit 
+          ?
+          <div className="flex justify-between">
+            <div>{comm.content}</div>
+            <div className="flex items-center gap-4">
+              <Pencil size={18} onClick={() => setIsEdit(true)} className="hover:cursor-pointer"/>
+              { 
+                <AlertDialog>
+                  <AlertDialogTrigger>
+                    <Trash2 size={18} className="hover:cursor-pointer"/>
                 
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                  <AlertDialogDescription>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                      <AlertDialogDescription>
                   This action cannot be undone. This will permanently delete your comment
                   and remove your data from our servers.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>Cancel</AlertDialogCancel>
-                  <AlertDialogAction variant="destructive" onClick={handleDelete}>Delete</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          }
-        </div>
-      </div>
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction variant="destructive" onClick={handleDelete}>Delete</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              }
+            </div>
+          </div>
+          : 
+          <form className="flex flex-col gap-2" onSubmit={handleEdit}>
+            <Input value={input} onChange={(e) => setInput(e.target.value)} />
+            <div className="flex justify-end gap-1">
+              <Button type="button" variant={"outline"} onClick={() => setIsEdit(false)}>Cancel</Button>
+              <Button type="submit" onClick={handleEdit}>Submit</Button>
+            </div>
+          </form>
+      }
     </div>
   );
 }
